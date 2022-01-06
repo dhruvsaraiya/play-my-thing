fc246b1c2cb5ac01b0f50ff7ed266936358deabd
Dhruv Saraiya <dhruvsaraiya30@gmail.com>
2022-01-07 00:57:18 +0530

    made some progress and learnt how to call JS functions from react. They are just pure functions assigned to window object, nothing complex here ;(

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,91 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Head from "next/head";
 import Script from "next/script";
 
+const PLAYER_DOM_ID = "youtube-player";
+const PLAYLIST_ID = "PLsa91shghYK8O5Yhqpb0NsPzucOtgQVTu";
+const VOLUME_STEP = 5;
+
 export default function Home() {
+  const [player, setPlayer] = useState(null);
+  const [volume, setVolume] = useState(0);
+
+  const onNext = () => {
+    player.nextVideo();
+  };
+  const onPrevious = () => {
+    player.previousVideo();
+  };
+  const onPause = () => {
+    player.pauseVideo();
+  };
+  const onPlay = () => {
+    player.playVideo();
+  };
+  const onMute = () => {
+    player.mute();
+  };
+  const onUnMute = () => {
+    player.unMute();
+  };
+  const onVolumeChange = (e) => {
+    setVolume(e.target.value);
+    e.preventDefault();
+  };
+  const increaseVolume = () => {
+    setVolume(volume + VOLUME_STEP);
+  };
+  const decreaseVolume = () => {
+    setVolume(volume - VOLUME_STEP);
+  };
+
+  const onPlayerReady = () => {
+    // not needed
+    // player.loadPlaylist({
+    //   list: PLAYLIST_ID,
+    //   listType: "playlist",
+    // });
+
+    // The order here is very important
+    player.mute();
+    player.playVideoAt(Math.floor(Math.random() * 100));
+    setTimeout(() => {
+      player.setShuffle(true);
+    }, 2000);
+  };
+
+  useEffect(() => {
+    if (!player) return;
+    if (volume > 0 && player.isMuted()) {
+      player.unMute();
+    }
+    player.setVolume(volume);
+  }, [volume]);
+
+  useEffect(() => {
+    if (!player) return;
+    player.addEventListener("onReady", onPlayerReady);
+  }, [player]);
+
+  useEffect(() => {
+    // This is the great learning for me, I was thinking that how do I communicate between React <> JS pages.
+    // Everything in dom world belongs to window, so whenever you place a function in JS file ( in my case onYouTubeIframeAPIReady in load-yt.js ) the function belongs to window object.
+    // So instead of putting it in load-yt.js file, I can put it over here with attached to window object :)
+
+    // The API will call this function when page has finished downloading
+    // the JavaScript for the player API.
+    window.onYouTubeIframeAPIReady = () => {
+      if (
+        window.YT &&
+        window.YT.Player &&
+        window.YT.Player instanceof Function
+      ) {
+        // keep the same name as iframe ID, so that new player doesn't pop-up
+        setPlayer(new window.YT.Player(PLAYER_DOM_ID));
+      }
+    };
+  }, []);
+
   return (
     <div className="container">
       <Head>
@@ -10,18 +93,36 @@ export default function Home() {
         <link rel="icon" href="/lofi.ico" />
       </Head>
 
-      {/* <div id="youtube-player" />
-      <Script src="load-yt.js" /> */}
+      <Script src="load-yt.js" />
       <iframe
+        id={PLAYER_DOM_ID}
         width="560"
         height="315"
-        src="https://www.youtube-nocookie.com/embed/videoseries?list=PLsa91shghYK8O5Yhqpb0NsPzucOtgQVTu&autoplay=1"
+        src={`https://www.youtube-nocookie.com/embed/videoseries?list=${PLAYLIST_ID}&autoplay=1&enablejsapi=1`}
         title="My playlist 💙"
         frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
+        allow="autoplay;encrypted-media;"
       />
-      <main />
+      <main>
+        <button type="button" onClick={onPrevious}>
+          Previous
+        </button>
+        <button type="button" onClick={onPause}>
+          Pause
+        </button>
+        <button type="button" onClick={onNext}>
+          Next
+        </button>
+      </main>
+
+      <input
+        type="range"
+        min="1"
+        max="100"
+        value={volume}
+        id="volume"
+        onChange={onVolumeChange}
+      />
 
       <style jsx>{`
         .container {
--- public/load-yt.js
+++ public/load-yt.js
@@ -1,42 +1,8 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
-// 2. This code loads the IFrame Player API code asynchronously.
-const tag = document.createElement('script');
 
+const tag = document.createElement("script");
+tag.id = "iframe-demo";
 tag.src = "https://www.youtube.com/iframe_api";
-const firstScriptTag = document.getElementsByTagName('script')[0];
+const firstScriptTag = document.getElementsByTagName("script")[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-// 3. This function creates an <iframe/> (and YouTube player)
-//    after the API code downloads.
-let player;
-function onPlayerReady(event) {
-    event.target.playVideo();
-}
-
-function stopVideo() {
-    player.stopVideo();
-  }
-
-  let done = false;
-function onPlayerStateChange(event) {
-  if (event.data === YT.PlayerState.PLAYING && !done) {
-    setTimeout(stopVideo, 6000);
-    done = true;
-  }
-}
-
-function onYouTubeIframeAPIReady() {
-  player = new YT.Player('youtube-player', {
-    height: '0',
-    width: '0',
-    videoId: 'M7lc1UVf-VE',
-    playerVars: {
-      'playsinline': 1
-    },
-    events: {
-      'onReady': onPlayerReady,
-      'onStateChange': onPlayerStateChange
-    }
-  });
-}
